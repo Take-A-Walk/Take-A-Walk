ecfa051652f5a0a1e055a1ed7f6a9f5300e552c0
babychoujr <babychoujr@gmail.com>
2021-02-16 14:30:26 -0800

    Placing User location on map

--- App.js
+++ App.js
@@ -1,15 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {Component}from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class App extends Component{
+  state = {
+    location: null,
+    errorMessage: null
+  };
+
+  //works for iOS emulator/emulator / Android device
+  //runs before the DOM actually loads
+  async componentDidMount(){
+    const {status } = await Location.requestPermissionsAsync();
+
+    if(status !== 'granted'){
+      const response = await Location.requestPermissionsAsync();
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  }
+
+
+
+  render(){
+    let text = 'Waiting..';
+    let lat = 0;
+    let long = 0;
+    if(this.state.errorMessage){
+      text = this.state.errorMessage
+    }
+    else if(this.state.location){
+      text = this.state.location.coords.latitude;
+      lat = this.state.location.coords.latitude;
+      long = this.state.location.coords.longitude;
+      //lat = JSON.stringify(this.state.location.coords.latitude);
+      //long = JSON.stringify(this.state.location.coords.longitude);
+    }
+      return (
+       <MapView 
+       provider = {PROVIDER_GOOGLE}
+       style = {styles.map} 
+       region = {{
+         latitude:  lat,
+         longitude: long,
+         latitudeDelta: 0.09,
+         longitudeDelta: 0.035
+      }}
+      showsUserLocation = {true}>
+        <MapView.Marker 
+          pinColor = {"blue"}
+          coordinate = {{
+            latitude: lat,
+            longitude: long
+          }}
+          title = {"MY CURRENT LOCATION"}
+          description = {"WHERE SHOULD I GO RUNNING TODAY?"}
+        />
+      </MapView>
+      
+      );
+    }
+  }
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +72,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome:{
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  map:{
+    height: '50%'
+  }
 });
