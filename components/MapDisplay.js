f1313fbd713be189fab9fd8826f027728059f7e8
babychoujr <babychoujr@gmail.com>
2021-02-17 23:25:41 -0800

    Figuring out how to code map + nearby areas

--- components/MapDisplay.js
+++ components/MapDisplay.js
@@ -1,98 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {Component}from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, TouchableHighlightBase, View } from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {Card } from 'react-native-elements';
 import * as Location from 'expo-location';
 
-export default class MapDisplay extends Component{
-  state = {
-    location: null,
-    errorMessage: null,
-    response:null
-  };
-
-  //works for iOS emulator/emulator / Android device
-  //runs before the DOM actually loads
-  async componentDidMount(){
-    const {status } = await Location.requestPermissionsAsync();
-
-    if(status !== 'granted'){
-      const response = await Location.requestPermissionsAsync();
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location });
-
-    this.fetchPlacesAPI(location.coords.latitude, location.coords.longitude);
-  }
-
-  fetchPlacesAPI = (lat, long) =>{
-      let radius = 20;
-
-      const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + ',' + long + '&radius=' + radius + '&key=' + "AIzaSyB0Ckjw0mGcuaUHHTIyx6FW_zqygm-ZIBM"
+export default function MapDisplay() {
   
-      let response = fetch(url);
+  const [location, setLocation] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [placeResponse, setPlaceResponse] = useState(null);
 
-      if(response.ok){
-        let rep = response.json();
-        this.setState({rep});
+  useEffect(() => {
+      (async () => {
+      try {
+          if (Platform.OS !== 'web') {
+              let { status } = await Location.requestPermissionsAsync();
+              if (status !== 'granted') {
+                  //alert('Sorry, we need camera roll permissions to make this work!');
+                  setErrorMsg('Sorry we need your location for the app to work!');
+                  return;
+              }
+              let location = await Location.getCurrentPositionAsync({});
+              setLocation(location);
+              setLatitude(location.coords.latitude);
+              setLongitude(location.coords.longitude);
+
+          }
+      }finally{
+
       }
-      else{
-        alert("HTTP ERROR: " + response.status);
-      }
+      })();
+    }, []);
+  //this.fetchPlacesAPI(location.coords.latitude, location.coords.longitude);
+  /*
+  fetchPlacesAPI = (lat, long) => {
 
-    }
+      let radius = 2000;
 
-  render(){
-    let text = 'Waiting..';
-    let lat = 0;
-    let long = 0;
-    if(this.state.errorMessage){
-      text = this.state.errorMessage
-    }
-    else if(this.state.location){
-      lat = this.state.location.coords.latitude;
-      long = this.state.location.coords.longitude;
-      text = JSON.stringify(this.state.response);
-      //lat = JSON.stringify(this.state.location.coords.latitude);
-      //long = JSON.stringify(this.state.location.coords.longitude);
-    }
-      return (
-        /*
-        <MapView.Marker 
-          pinColor = {"blue"}
-          coordinate = {{
-            latitude: lat,
-            longitude: long
-          }}
-          title = {"MY CURRENT LOCATION"}
-          description = {"WHERE SHOULD I GO RUNNING TODAY?"}
-        />
-      </MapView>
-      */
-      <View style = {styles.container}>
-        <MapView 
-       provider = {PROVIDER_GOOGLE}
-       style = {styles.map} 
-       region = {{
-         latitude:  lat,
-         longitude: long,
-         latitudeDelta: 0.09,
-         longitudeDelta: 0.035
-      }}
-      showsUserLocation = {true}></MapView>
-       <Text>text</Text>
-     </View>
+      const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + ',' + long + '&radius=' + radius + '&type=park' + '&key=' + "AIzaSyB0Ckjw0mGcuaUHHTIyx6FW_zqygm-ZIBM"
 
-      
-      );
-    }
+      fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          
+          let places = [] // hold the places
+          for(let googlePlace of data.results){
+            let place = {}
+
+            let coords = {
+              latitude: googlePlace.geometry.location.lat,
+              longitude: googlePlace.geometry.location.lng
+            }
+
+            place['coords'] = coords
+
+            places.push(place)
+          }
+
+          setPlaceResponse(places);
+        })
+        .catch(console.error)
   }
+  */
+
+  let text = "Waiting...";
+  let lat = 0;
+  let long = 0;
+  let response = [];
+  if (errorMsg) {
+      text = "ERROR";
+   } 
+  else if (location) {
+
+      lat = latitude;
+      long = longitude;
+  }
+  showMap = (lat, long) =>{
+    return(
+      <MapView 
+      provider = {PROVIDER_GOOGLE}
+      style = {styles.map} 
+      region = {{
+        latitude:  lat,
+      longitude: long,
+      latitudeDelta: 0.09,
+      longitudeDelta: 0.035
+    }}
+    showsUserLocation = {true}>
+      <MapView.Marker
+        coordinate={{latitude: 37.78825,
+        longitude: -110.342}}
+        title={"title"}
+        description={"description"}
+     />
+    </MapView>);
+
+  }
+  return(
+    <View>
+        {this.showMap(lat, long)}
+      </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -102,6 +118,6 @@ const styles = StyleSheet.create({
     margin: 10
   },
   map:{
-    height: '50%'
+    height: '80%'
   }
 });
