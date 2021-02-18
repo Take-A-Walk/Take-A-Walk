125d4a549be6d4402832b6ee33467291c2162bf0
dogurasu <dugs134@gmail.com>
2021-02-18 00:45:58 -0800

    Added some forms

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import GLOBAL from './global.js'
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -7,18 +7,45 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import LoginForm from './components/LoginForm';
 import Homepage from './components/Homepage';
 import ProfileScreen from './components/ProfileScreen';
 import MapScreen from './components/MapScreen';
 
+import firebase from './Firebase';
+require
+
 const Stack = createStackNavigator();
 
 export default function App() {
+    
+    useEffect(() => {
+        // create reference - dbRefObj points at the location object
+        const dbRefObj = firebase.database().ref().child('users');
+
+        // sync object changes
+        dbRefObj.on('value', snap => console.log(snap.val()));
+
+        // dbRefObj.get()
+        //     .then((snap) => {
+        //         console.log(snap.val());
+        //     })
+        //     .catch((err) => {
+        //         console.log(err);
+        //     })
+
+        // console.log(dbRefObj);
+    }, [])
 
     return (
     <SafeAreaProvider>
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen
+                name="Login"
+                component={LoginForm}
+                options={{ title: 'Login' }}
+                />
                 <Stack.Screen
                 name="Home"
                 component={Homepage}
---
+++ components/common/Input.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+const Input = ({ label, value, onChangeText, placeholder, pwObstruct }) => {
+    const { inputField, labelStyle, containerStyle } = styles;
+
+    useEffect(() => {
+        console.log('Input rendered!');
+    })
+
+    return (
+        <View style={containerStyle}>
+            <Text style={labelStyle}>{label}</Text>
+            <TextInput
+                value={value}
+                style={inputField}
+                placeholder={placeholder}
+                autoCorrect={false}
+                onChangeText={onChangeText}
+                secureTextEntry={pwObstruct}
+            />
+        </View>
+    );
+};
+
+const styles = {
+    inputField: {
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 20,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 3
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    },
+    containerStyle: {
+        height: 50,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+};
+
+export { Input };
